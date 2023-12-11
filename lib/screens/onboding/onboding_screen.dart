cc908ba0a4a917cbced8638a6b7e1b2c1144cd1d
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-12-12 00:36:59 +0530

    add custom sign in dialog

--- lib/screens/onboding/onboding_screen.dart
+++ lib/screens/onboding/onboding_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
-import 'package:animated_app/screens/onboding/components/sign_in_form.dart';
+import 'package:animated_app/screens/onboding/components/custom_sign_in_dialog.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rive/rive.dart';
 
 import 'components/animated_btn.dart';
@@ -14,13 +13,14 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  bool isSignInDialogShown = false;
   late RiveAnimationController _btnAnimationController;
 
   @override
   void initState() {
     _btnAnimationController = OneShotAnimation(
       "active",
-      autoplay: true, //false
+      autoplay: false,
     );
     super.initState();
   }
@@ -46,160 +46,67 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
             child: const SizedBox(),
           )),
-          SafeArea(
-              child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Spacer(),
-                const SizedBox(
-                  width: 260,
-                  child: Column(
-                    children: [
-                      Text(
-                        "Learn design & codes",
-                        style: TextStyle(
-                          fontSize: 60,
-                          fontFamily: "Poppins",
-                          height: 1.2,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Text(
-                          "Don't skip design. Learn design and code, by building real apps with Flutter and Swift. Complete courses about the best tools."),
-                    ],
-                  ),
-                ),
-                const Spacer(
-                  flex: 2,
-                ),
-                AnimatedBtn(
-                  btnAnimationController: _btnAnimationController,
-                  press: () {
-                    _btnAnimationController.isActive = true;
-                    customSigninDialog(context);
-                  },
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 24),
-                  child: Text(
-                      "Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates."),
-                )
-              ],
-            ),
-          ))
-        ],
-      ),
-    );
-  }
-
-  Future<Object?> customSigninDialog(BuildContext context) {
-    return showGeneralDialog(
-      barrierDismissible: true,
-      barrierLabel: "Sign In",
-      context: context,
-      pageBuilder: (context, _, __) => Center(
-        child: Container(
-          height: 620,
-          margin: const EdgeInsets.symmetric(horizontal: 16),
-          padding: const EdgeInsets.symmetric(
-            vertical: 32,
-            horizontal: 24,
-          ),
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.94),
-            borderRadius: const BorderRadius.all(Radius.circular(40)),
-          ),
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            body: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Column(
-                  children: [
-                    const Text(
-                      "Sign In",
-                      style: TextStyle(fontSize: 34, fontFamily: "Poppins"),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16),
-                      child: Text(
-                        "Access to 240+ hours of content. Learn design and code, by building real apps with Flutter and Swift.",
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    const SignInForm(),
-                    const Row(
+          AnimatedPositioned(
+            top: isSignInDialogShown ? -50 : 0,
+            duration: const Duration(milliseconds: 240),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: SafeArea(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Spacer(),
+                  const SizedBox(
+                    width: 260,
+                    child: Column(
                       children: [
-                        Expanded(child: Divider()),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          child: Text(
-                            "OR",
-                            style: TextStyle(color: Colors.black26),
+                        Text(
+                          "Learn design & codes",
+                          style: TextStyle(
+                            fontSize: 60,
+                            fontFamily: "Poppins",
+                            height: 1.2,
                           ),
                         ),
-                        Expanded(child: Divider()),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                            "Don't skip design. Learn design and code, by building real apps with Flutter and Swift. Complete courses about the best tools."),
                       ],
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 24),
-                      child: Text(
-                        "Sign up with Email, Apple or Google",
-                        style: TextStyle(color: Colors.black54),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        IconButton(
-                            padding: EdgeInsets.zero,
-                            onPressed: () {},
-                            icon: SvgPicture.asset(
-                              "assets/icons/email_box.svg",
-                              height: 64,
-                              width: 64,
-                            )),
-                        IconButton(
-                            padding: EdgeInsets.zero,
-                            onPressed: () {},
-                            icon: SvgPicture.asset(
-                              "assets/icons/apple_box.svg",
-                              height: 64,
-                              width: 64,
-                            )),
-                        IconButton(
-                            padding: EdgeInsets.zero,
-                            onPressed: () {},
-                            icon: SvgPicture.asset(
-                              "assets/icons/google_box.svg",
-                              height: 64,
-                              width: 64,
-                            )),
-                      ],
-                    )
-                  ],
-                ),
-                const Positioned(
-                  left: 0,
-                  right: 0,
-                  bottom: -48,
-                  child: CircleAvatar(
-                    radius: 16,
-                    backgroundColor: Colors.white,
-                    child: Icon(
-                      Icons.close,
-                      color: Colors.black,
                     ),
                   ),
-                )
-              ],
-            ),
-          ),
-        ),
+                  const Spacer(
+                    flex: 2,
+                  ),
+                  AnimatedBtn(
+                    btnAnimationController: _btnAnimationController,
+                    press: () {
+                      _btnAnimationController.isActive = true;
+                      Future.delayed(const Duration(milliseconds: 800), () {
+                        setState(() {
+                          isSignInDialogShown = true;
+                        });
+                        customSigninDialog(context, onClosed: (_) {
+                          setState(() {
+                            isSignInDialogShown = false;
+                          });
+                        });
+                      });
+                    },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 24),
+                    child: Text(
+                        "Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates."),
+                  )
+                ],
+              ),
+            )),
+          )
+        ],
       ),
     );
   }
