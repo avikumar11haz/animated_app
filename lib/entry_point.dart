96c097b9fb0056a1f14591a71eeae13806c82d66
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-12-15 20:34:26 +0530

    gesture detector

--- lib/entry_point.dart
+++ lib/entry_point.dart
@@ -11,7 +11,7 @@ class EntryPoint extends StatefulWidget {
 }
 
 class _EntryPointState extends State<EntryPoint> {
-  late SMIBool searchTigger;
+  RiveAsset selectedBottomNav = bottomNavs.first;
 
   @override
   Widget build(BuildContext context) {
@@ -31,27 +31,72 @@ class _EntryPointState extends State<EntryPoint> {
                   bottomNavs.length,
                   (index) => GestureDetector(
                         onTap: () {
-                          searchTigger.change(true);
+                          bottomNavs[index].input!.change(true);
+                          if (bottomNavs[index] != selectedBottomNav) {
+                            setState(() {
+                              selectedBottomNav = bottomNavs[index];
+                            });
+                          }
+                          Future.delayed(const Duration(seconds: 1), () {
+                            bottomNavs[index].input!.change(true);
+                          });
                         },
-                        child: SizedBox(
-                          height: 36,
-                          width: 36,
-                          child: RiveAnimation.asset(
-                            bottomNavs.first.src,
-                            artboard: "SEARCH",
-                            onInit: (artboard) {
-                              StateMachineController controller =
-                                  RiveUtils.getRiveController(artboard,
-                                      stateMachineName: "SEARCH_Interactivity");
-                              searchTigger =
-                                  controller.findSMI("active") as SMIBool;
-                            },
-                          ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            AnimatedBar(
+                                isActive:
+                                    bottomNavs[index] == selectedBottomNav),
+                            SizedBox(
+                              height: 36,
+                              width: 36,
+                              child: Opacity(
+                                opacity: bottomNavs[index] == selectedBottomNav
+                                    ? 1
+                                    : 0.5,
+                                child: RiveAnimation.asset(
+                                  bottomNavs.first.src,
+                                  artboard: bottomNavs[index].artboard,
+                                  onInit: (artboard) {
+                                    StateMachineController controller =
+                                        RiveUtils.getRiveController(artboard,
+                                            stateMachineName: bottomNavs[index]
+                                                .stateMachineName);
+                                    bottomNavs[index].input =
+                                        controller.findSMI("active") as SMIBool;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ))
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class AnimatedBar extends StatelessWidget {
+  const AnimatedBar({
+    super.key,
+    required this.isActive,
+  });
+
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      margin: const EdgeInsets.only(bottom: 2),
+      height: 4,
+      width: isActive ? 20 : 0,
+      decoration: const BoxDecoration(
+        color: Color(0xFF81B4FF),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
       ),
     );
   }
@@ -70,27 +115,25 @@ class RiveAsset {
   set setInput(SMIBool status) {
     input = status;
   }
-
-  List<RiveAsset> bottomNavs = [
-    RiveAsset("assets/RiveAssets/icons.riv",
-        artboard: "CHAT",
-        stateMachineName: "CHAT_Interactivity",
-        title: "Chat"),
-    RiveAsset("assets/RiveAssets/icons.riv",
-        artboard: "SEARCH",
-        stateMachineName: "SEARCH_Interactivity",
-        title: "Search"),
-    RiveAsset("assets/RiveAssets/icons.riv",
-        artboard: "TIMER",
-        stateMachineName: "TIMER_Interactivity",
-        title: "Chat"),
-    RiveAsset("assets/RiveAssets/icons.riv",
-        artboard: "BELL",
-        stateMachineName: "BELL_Interactivity",
-        title: "Notifications"),
-    RiveAsset("assets/RiveAssets/icons.riv",
-        artboard: "USER",
-        stateMachineName: "USER_Interactivity",
-        title: "Profile"),
-  ];
 }
+
+List<RiveAsset> bottomNavs = [
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "CHAT", stateMachineName: "CHAT_Interactivity", title: "Chat"),
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "SEARCH",
+      stateMachineName: "SEARCH_Interactivity",
+      title: "Search"),
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "TIMER",
+      stateMachineName: "TIMER_Interactivity",
+      title: "Chat"),
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "BELL",
+      stateMachineName: "BELL_Interactivity",
+      title: "Notifications"),
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "USER",
+      stateMachineName: "USER_Interactivity",
+      title: "Profile"),
+];
